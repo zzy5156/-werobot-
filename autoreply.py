0dcc950b7ee4e007e6c5193fc474a0082d127980
zzy5156 <49281709+zzy5156@users.noreply.github.com>
2019-08-22 19:28:00 +0800

    1.2
    修复了彩云api不可用时报错退出的问题
    加入查询任意地方天气，使用方法为在公众号发送位置，使用心知天气数据源（因为werobot返回的是纬度，经度，而彩云天气需要经度，纬度才能查询）
    彩云api不可用时，使用中国天气网的数据

--- autoreply.py
+++ autoreply.py
@@ -9,15 +9,34 @@ import time
 import random
 import requests
 import werobot
-robot=werobot.WeRoBot(token='输入微信公众号的token')
+robot=werobot.WeRoBot(token='ziyun')
 
 @robot.filter('天气')
 def weather():
-    json_text = requests.get(str.format("https://api.caiyunapp.com/v2/输入彩云天气的token/输入经纬度/minutely.json")).content
+    json_text = requests.get(str.format("https://api.caiyunapp.com/v2/输入你的彩云天气api_token/输入经纬度/minutely.json")).content
     self_realtime_data = json.loads(json_text)
-    now_data=self_realtime_data['result']['forecast_keypoint']
-    nowTime=datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-    send_data="降雨预报："+now_data+"\n更新于："+nowTime
+    status=self_realtime_data['status']
+    if status!='failed':
+        now_data=self_realtime_data['result']['forecast_keypoint']
+        nowTime=datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        send_data="降雨预报："+now_data+"\n更新于："+nowTime+"\n详情请访问www.caiyunapp.com/map"
+    else:
+        send_data="彩云API已经到配额上限，无法查询。\n可访问www.caiyunapp.com/map查看天气。"
+    return send_data
+
+@robot.location
+def place(message,location):
+    place=message.location
+    n_place=":".join([str(i) for i in place])
+    print (n_place)
+    json_text = requests.get(str.format("https://api.seniverse.com/v3/weather/now.json?key=输入你的心知天气api_token&location="+n_place+"&language=zh-Hans&unit=c")).content
+    r_data = json.loads(json_text)
+    data=r_data['results'][0]
+    wea=data['now']['text']
+    tem=data['now']['temperature']
+    location=data['location']['name']
+    utime=data['last_update']
+    send_data=location+"，"+wea+"，"+tem+"℃ \n更新时间："+utime+"\n数据来源：心知天气"+"\n若想查看您所在地的天气，可访问www.caiyunapp.com/map"
     return send_data
 
 robot.config['HOST']='0.0.0.0'
--- caiyun.py
+++ caiyun.py
@@ -9,7 +9,7 @@ import json
 import time
 def sleeptime(hour,min,sec):
     return hour*3600 + min*60 + sec;
-second = sleeptime(0,30,0);#30分钟刷新一次
+second = sleeptime(0,30,0);
 def get_access_token():
     """
     获取微信全局接口的凭证(默认有效期俩个小时)
@@ -19,8 +19,8 @@ def get_access_token():
         url="https://api.weixin.qq.com/cgi-bin/token",
         params={
             "grant_type": "client_credential",
-            "appid": "输入你的公众号appid",
-            "secret": "输入你的公众号的appid_secret",
+            "appid": "输入你的appid",
+            "secret": "输入你的appidsecret",
         }
     ).json()
 
@@ -37,7 +37,7 @@ def sendmsg(openid,msg):
     body = {
         "touser": openid,
         "template_id":"输入你的模板id",
-        "url":"www.baidu.com",
+        "url":"www.caiyunapp.com/map",
         "data": {
             "weather":{
             "value": msg,
@@ -54,14 +54,40 @@ def sendmsg(openid,msg):
     result = response.json()
     print(result)
 while True: 
-    json_text = requests.get(str.format("https://api.caiyunapp.com/v2/输入你的api/输入查询地方的经纬度/minutely.json")).content
+    json_text = requests.get(str.format("https://api.caiyunapp.com/v2/输入你的彩云天气api_token/输入经纬度，用英文逗号分隔/minutely.json")).content
     self_realtime_data = json.loads(json_text)
-    now_data=self_realtime_data['result']['forecast_keypoint']
-    print ("降雨预报："+now_data)
-    nowTime=datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-    print ("更新于："+nowTime)
-    send_data="\n"+now_data+"\n更新于："+nowTime
+    status=self_realtime_data['status']
+    json_text=requests.get(str.format("https://www.tianqiapi.com/api/?version=v6&city=输入城市名")).content
+    data = json.loads(json_text)
+    cityid=data['cityid']
+    city=data['city']
+    date=data['date']
+    utime=data['update_time']
+    week=data['week']
+    wea=data['wea']
+    h_tem=data['tem1']
+    l_tem=data['tem2']
+    n_tem=data['tem']
+    win=data['win']
+    win_speed=data['win_speed']
+    win_meter=data['win_meter']
+    hum=data['humidity']
+    visit=data['visibility']
+    pressure=data['pressure']
+    air=data['air']
+    pm25=data['air_pm25']
+    air_level=data['air_level']
+    air_tips=data['air_tips']
+    alarm=data['alarm']['alarm_content']
+    alarm_type=data['alarm']['alarm_type']
+    alarm_level=data['alarm']['alarm_level']
+    if status=='failed':
+        send_data=city+"天气预报：\n"+"\n天气："+wea+"  "+n_tem+"℃\n最高/低温："+h_tem+"℃ /"+l_tem+"℃\n湿度："+hum+"\n"+win+"  "+win_speed+"  "+win_meter+"\n能见度："+visit+"\n空气质量："+air+"  "+air_level+"  "+air_tips+"\npm2.5："+pm25+"\n预警消息："+alarm+"\n更新时间："+date+" "+week+" "+utime+"\n数据来源：中国天气网\n当您看到这条消息时，彩云天气api已经到达配额上限，请点击详情手动查询降雨预报"
+    else: 
+        now_data=self_realtime_data['result']['forecast_keypoint']
+        nowTime=datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        send_data="降雨预报：\n"+now_data+"\n更新于："+nowTime+"\n天气："+wea+"  "+n_tem+"℃\n最高/低温："+h_tem+"℃ /"+l_tem+"℃\n湿度："+hum+"\n"+win+"  "+win_speed+"  "+win_meter+"\n能见度："+visit+"\n空气质量："+air+"  "+air_level+"  "+air_tips+"\npm2.5："+pm25+"\n预警消息："+alarm+"\n数据来源：彩云天气、中国天气网"
     if __name__ == '__main__':
-        sendmsg('输入接收信息的id',send_data)
+        sendmsg('输入接收信息的微信号',send_data)
     time.sleep(second);
 
